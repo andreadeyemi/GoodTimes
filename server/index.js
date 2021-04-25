4ffd365af413afa5b12c19499ef5c47718773adc
andre <andre@andres-MacBook-Pro.local>
2021-04-25 12:54:00 -0400

    Second commit

--- server/index.js
+++ server/index.js
@@ -2,22 +2,32 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dontenv from 'dotenv';
 import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts' , postRoutes);
+dontenv.config();
+app.use(cors());
+
+
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use('/posts' , postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://andreadeyemi:Taqwa2021@cluster0.9wprx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API')
+});
+
+
+// const CONNECTION_URL = 'mongodb+srv://andreadeyemi:Taqwa2021@cluster0.9wprx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
+
+
